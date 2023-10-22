2aea96dbe05a40d7c1087632a87e4214024a5a28
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-22 12:57:39 +0100

    fizzBuzz

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,7 @@
-const fizzBuzz = (number) => {};
+const fizzBuzz = (number) => {
+    return (number%3===0 && number%5!=0)? 'Fizz': (number%3!=0 && number%5===0)? 'Buzz': (number%3===0 && number%5===0)? 'FizzBuzz': number
+};
 
 module.exports = fizzBuzz;
+
+console.log(fizzBuzz(4))
