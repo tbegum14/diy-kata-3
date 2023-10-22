67cd4cb2bfd935b33860cde1a391ab1a3f604f7f
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-22 14:46:44 +0100

    numberToReveresedDigits complete

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,11 @@
-const numberToReversedDigits = (number) => {};
+const numberToReversedDigits = (number) => {
+    const array = number.toString().split('').reverse()
+    const array2 = []
+    for (let i=0; i<array.length; i++){
+        array2.push(Number(array[i]))
+    }
+    return array2
+};
 
 module.exports = numberToReversedDigits;
+
