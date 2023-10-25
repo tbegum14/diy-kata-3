3547eb79e1b6429091c9845ca11720e560795ba4
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-25 10:40:30 +0100

    2 challenges added

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,18 @@
-const humanCatDogYears = (number) => {};
-
+const humanCatDogYears = (number) => {
+    const ageArray = [number]
+    if (number===1){
+        ageArray.push(15)
+        ageArray,push(15)
+    }
+    else if (number===2){
+        ageArray.push(24)
+        ageArray.push(24)
+    }
+    else {
+        ageArray.push(24+4*(number-2))
+        ageArray.push(24+5*(number-2))
+    }
+    return ageArray
+}; 
 module.exports = humanCatDogYears;
+
--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,9 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const time = distance/speed
+    const decimal = time - Math.floor(time)
+    return decimal < 0.75 && decimal > 0.25? Math.floor(time)+0.5:Math.round(time)
+};
 
 module.exports = reachDestination;
+
+
